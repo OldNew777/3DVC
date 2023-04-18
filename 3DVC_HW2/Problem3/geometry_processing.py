674426a85afc5e9e8cc82d1256b08f948c363f3f
chenxin <xin-chen22@mails.tsinghua.edu.cn>
2023-04-18 19:06:35 +0800

    finsih constraints visulization

--- 3DVC_HW2/Problem3/geometry_processing.py
+++ 3DVC_HW2/Problem3/geometry_processing.py
@@ -1,17 +1,71 @@
-from func import *
-from mylogger import logger
 import os
 import trimesh
 import numpy as np
 from typing import Tuple
 
+from func import *
+from mylogger import logger
 
-@time_it
-def load_point_cloud(filename) -> Tuple[np.ndarray, np.ndarray]:
-    mesh = trimesh.load(filename)
-    v = np.array(mesh.vertices)
-    n = np.zeros_like(v)
-    data = mesh.metadata['_ply_raw']['vertex']['data']
-    for i in range(len(data)):
-        n[i] = [data[i]['nx'], data[i]['ny'], data[i]['nz']]
-    return v, n
+
+class PointCloud:
+    def __init__(self, v: np.ndarray, n: np.ndarray = None, color: np.ndarray = None):
+        self.v = v
+        self.n = n
+        self.color = 255 * np.ones(shape=(v.shape[0], 3), dtype=int) if color is None else color
+        assert len(self.v) == len(self.color)
+
+    def __len__(self):
+        return len(self.v)
+
+    def __add__(self, other):
+        return PointCloud(
+            np.concatenate((self.v, other.v), axis=0),
+            np.concatenate((self.n, other.n), axis=0) if self.n is not None and other.n is not None else None,
+            np.concatenate((self.color, other.color), axis=0) if self.color is not None and other.color is not None else None,
+        )
+
+    @classmethod
+    def from_ply(cls, filename):
+        mesh = trimesh.load(filename)
+        v = np.array(mesh.vertices)
+        n = np.zeros_like(v)
+        data = mesh.metadata['_ply_raw']['vertex']['data']
+        for i in range(len(data)):
+            n[i] = [data[i]['nx'], data[i]['ny'], data[i]['nz']]
+        return cls(v, n)
+
+    def export_ply(self, filename):
+        try:
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+        except:
+            pass
+
+        normal_exist = self.n is not None and len(self.n) == len(self.v)
+        with open(filename, 'w') as file:
+            file.writelines([
+                'ply\n',
+                'format ascii 1.0\n',
+                'comment author: Xin Chen\n',
+                f'element vertex {len(self.v)}\n',
+                'property float x\n',
+                'property float y\n',
+                'property float z\n',
+            ])
+            if normal_exist:
+                file.writelines([
+                    'property float nx\n',
+                    'property float ny\n',
+                    'property float nz\n',
+                ])
+            file.writelines([
+                'property uchar red\n',
+                'property uchar green\n',
+                'property uchar blue\n',
+                'end_header\n',
+            ])
+
+            for i in range(len(self)):
+                file.write(
+                    f'{self.v[i][0]} {self.v[i][1]} {self.v[i][2]} '
+                    + (f'{self.n[i][0]} {self.n[i][1]} {self.n[i][2]} ' if normal_exist else '') +
+                    f'{self.color[i][0]} {self.color[i][1]} {self.color[i][2]}\n')
--- 3DVC_HW2/Problem3/get_constraints.py
+++ 3DVC_HW2/Problem3/get_constraints.py
@@ -1,13 +1,49 @@
 import numpy as np
+from sklearn import neighbors, decomposition
 
 from geometry_processing import *
 
 
-def get_constraints(v: np.ndarray, n: np.ndarray):
-    pass
+def get_constraints(point_cloud: PointCloud, epsilon: float = 0.01):
+    kdtree = neighbors.KDTree(point_cloud.v)
+
+    def get_neighbor_constraint(index: int, epsilon: float) -> Tuple[np.ndarray, float]:
+        ep = epsilon
+        while True:
+            p = point_cloud.v[index] + ep * point_cloud.n[index]
+            neighbor_index = kdtree.query(p.reshape(1, -1), k=1, return_distance=False)[0][0]
+            if neighbor_index == index:
+                break
+            ep *= 0.5
+        return p, ep
+
+    v_new = np.zeros(shape=(2 * point_cloud.v.shape[0], 3), dtype=float)
+    n_new = np.zeros(shape=(2 * point_cloud.v.shape[0], 3), dtype=float)
+    color_new = np.zeros(shape=(2 * point_cloud.v.shape[0], 3), dtype=int)
+    p = np.zeros(shape=(3 * point_cloud.v.shape[0], 3), dtype=float)
+    values = np.zeros(shape=(3 * point_cloud.v.shape[0], 1), dtype=float)
+    for i in range(len(point_cloud)):
+        # constraint (a)
+        p[3 * i], values[3 * i] = point_cloud.v[i], 0
+
+        # constraint (b)
+        p[3 * i + 1], values[3 * i + 1] = get_neighbor_constraint(i, epsilon)
+        v_new[2 * i] = p[3 * i + 1]
+        n_new[2 * i] = point_cloud.n[i]
+        color_new[2 * i] = [0, 255, 0]
+
+        # constraint (c)
+        p[3 * i + 2], values[3 * i + 2] = get_neighbor_constraint(i, -epsilon)
+        v_new[2 * i + 1] = p[3 * i + 2]
+        n_new[2 * i + 1] = point_cloud.n[i]
+        color_new[2 * i + 1] = [255, 0, 0]
+
+    point_cloud += PointCloud(v_new, n_new, color_new)
+    point_cloud.export_ply('bunny-constraints.ply')
+    return point_cloud, p, values
 
 
 if __name__ == '__main__':
     filename = 'bunny.ply'
-    v, n = load_point_cloud(filename)
-    get_constraints(v, n)
+    point_cloud = PointCloud.from_ply(filename)
+    point_cloud, p, values = get_constraints(point_cloud, 0.01)
